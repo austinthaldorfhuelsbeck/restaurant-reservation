ba9d3da6fecc3a44eb80dae49a608922eb546d9a
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-05-21 14:48:09 -0700

    automatically run migrations

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,12 +5,12 @@ const knex = require("./db/connection")
 
 const listener = () => console.log(`Listening on Port ${PORT}!`)
 
-app.listen(PORT, listener)
+// app.listen(PORT, listener)
 
-// knex.migrate
-//   .latest()
-//   .then((migrations) => {
-//     console.log("migrations", migrations);
-//     app.listen(PORT, listener);
-//   })
-//   .catch(console.error);
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations)
+    app.listen(PORT, listener)
+  })
+  .catch(console.error)
