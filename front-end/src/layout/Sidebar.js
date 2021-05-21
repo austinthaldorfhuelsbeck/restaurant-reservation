7b12fec189f726edf20ae5b8b9ec508fb5d47fcb
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-05-21 10:52:31 -0700

    consolidate

--- front-end/src/layout/Sidebar.js
+++ front-end/src/layout/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// TODO: consolidate this import
+// TODO: import better
 import {
   faThumbtack,
   faCompass,
